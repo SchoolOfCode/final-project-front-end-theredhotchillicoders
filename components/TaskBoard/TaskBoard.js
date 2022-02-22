e5566584d194d96f1c72d7a979777ad05502537b
troywebber <troywebb95@gmail.com>
2022-02-22 16:55:11 +0000

    linked taskboard to database

--- components/TaskBoard/TaskBoard.js
+++ components/TaskBoard/TaskBoard.js
@@ -2,14 +2,14 @@ import React from "react";
 import css from "./Taskboard.module.css";
 import AddTask from "../addTaskButton/addTask.js";
 
-const todos = [
-  { title: "hello" },
-  { title: "cook" },
-  { title: "clean" },
-  { title: "wash" },
-  { title: "run" },
-  { title: "run" },
-];
+// const todos = [
+//   { title: "hello" },
+//   { title: "cook" },
+//   { title: "clean" },
+//   { title: "wash" },
+//   { title: "run" },
+//   { title: "run" },
+// ];
 
 function Todo({ todo }) {
   return (
@@ -20,15 +20,20 @@ function Todo({ todo }) {
   );
 }
 
-export default function TaskBoard() {
-  return (
-    <div className={css.taskboard}>
-      <div className={css.todoList}>
-        {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
-        ))}
+export default function TaskBoard({ todos }) {
+  console.log(todos);
+  if (todos.length > 0) {
+    return (
+      <div className={css.taskboard}>
+        <div className={css.todoList}>
+          {todos.map((todo, index) => (
+            <Todo key={index} index={index} todo={todo} />
+          ))}
+        </div>
+        <AddTask />
       </div>
-      <AddTask />
-    </div>
-  );
+    );
+  } else {
+    return <div>Loading</div>;
+  }
 }
