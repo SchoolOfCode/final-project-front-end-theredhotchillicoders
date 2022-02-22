c09d19be0ef8a8025d91f606781cb6b836ed1000
troywebber <troywebb95@gmail.com>
2022-02-22 10:59:19 +0000

    added grid layout for taskboard and progress bar

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,48 @@
-import NavBar from '../components/NavBar/NavBar';
-import React from 'react';
-import { useEffect, useState } from 'react';
-import ProgressBar from '../components/ProgressBar/ProgressBar';
+import NavBar from "../components/NavBar/NavBar";
+import React from "react";
+import { useEffect, useState } from "react";
+import ProgressBar from "../components/ProgressBar/ProgressBar";
+import TaskBoard from "../components/TaskBoard/TaskBoard";
+import css from "../styles/index.module.css";
 
 export default function Dashboard({ toggleColorMode, theme }) {
-	const [ taskComplete, setTaskComplete ] = useState();
+  const [taskComplete, setTaskComplete] = useState();
 
-	useEffect(() => {
-		async function fetchData() {
-			const response = await fetch(`https://socfinalproject.herokuapp.com/activities`);
-			const data = await response.json();
-			console.log(data);
-		}
-		fetchData();
-	}, []);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://socfinalproject.herokuapp.com/activities`
+      );
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchData();
+  }, []);
 
-	return (
-		<div>
-			<NavBar theme={theme} />
-			<h1>Dashboard</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam laborum deserunt consequatur,
-				assumenda quos. Aspernatur placeat ex error provident unde velit temporibus libero. Quaerat, temporibus
-				tenetur dignissimos perspiciatis libero quasi! Eaque maxime fugiat nesciunt accusamus dolorum eligendi
-				maiores reprehenderit quibusdam voluptate, aperiam, quia ad dolore. Provident minima similique aliquid?
-			</p>
-			<button onClick={toggleColorMode}>Light/dark mode</button>
-			<input type="number" onChange={(e) => setTaskComplete(e.target.value)} />
-			<ProgressBar TaskPercent={taskComplete} />
-		</div>
-	);
-
+  return (
+    <div>
+      <NavBar theme={theme} />
+      <h1>Dashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam
+        laborum deserunt consequatur, assumenda quos. Aspernatur placeat ex
+        error provident unde velit temporibus libero. Quaerat, temporibus
+        tenetur dignissimos perspiciatis libero quasi! Eaque maxime fugiat
+        nesciunt accusamus dolorum eligendi maiores reprehenderit quibusdam
+        voluptate, aperiam, quia ad dolore. Provident minima similique aliquid?
+      </p>
+      <button onClick={toggleColorMode}>Light/dark mode</button>
+      <input type="number" onChange={(e) => setTaskComplete(e.target.value)} />
+      <div className={css.container}>
+        <div className="Taskboard">
+          <TaskBoard />
+        </div>
+        <div className="Progress-Bar">
+          <ProgressBar TaskPercent={taskComplete} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 /* Home page components:
