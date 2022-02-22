6f2d1bebdc06b3fe8806b074193ef64d6569f54b
Pokemom3 <tanmatthews1@gmail.com>
2022-02-22 10:00:39 +0000

    created component structure Co-authored-by: Leanne leannesmith251@gmail.com Co-authored-by: Tan tanmatthews1@gmail.com Co-authored-by: Emily emy.sarina@gmail.com Co-authored-by: Mohammed ali.m2804@outlook.com Co-authored-by: troy troywebb95@gmail.com Co-authored-by: jack jackfirkin@gmail.com

--- components/ActivityButton/ActivityButton.js
+++ components/ActivityButton/ActivityButton.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from '@mui/material';
+import {
+  Button,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
+function ActivityButton({ title, description, category, setFitnessInfo }) {
+  function getExercise() {
+    setFitnessInfo({
+      title: title,
+      category: category,
+      description: description,
+    });
+  }
 
-  function ActivityButton({ title, description, category, setFitnessInfo }) {
-	function getExercise() {
-		setFitnessInfo({ title: title, category: category, description: description });
-	}
+  return (
+    <Grid item xs={12} sm={6} md={3}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea onClick={getExercise}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
 
-	return (
-		<Button sx={{ p: 5, m: 1, width: 30, height: 30 }} variant="contained" onClick={getExercise}>
-			{title}
-		</Button>
-	);
-
-
-
-
-
-	
+  {
+    /* <Button
+        sx={{ p: 15, m: 1, width: 30, height: 30 }}
+        variant="contained"
+        onClick={getExercise}
+      >
+        {title}
+      </Button>
+    </Grid> 
+  );*/
+  }
 }
 export default ActivityButton;
