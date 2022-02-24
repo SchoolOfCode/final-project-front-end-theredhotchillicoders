b01ebc5998e82c214810e1a94a8ba6e91ff8d6e1
Pokemom3 <tanmatthews1@gmail.com>
2022-02-24 12:14:42 +0000

    Co-authored-by: Leanne leannesmith251@gmail.com Co-authored-by: Tan tanmatthews1@gmail.com Co-authored-by: Emily emy.sarina@gmail.com Co-authored-by: Mohammed ali.m2804@outlook.com Co-authored-by: troy troywebb95@gmail.com Co-authored-by: jack jackfirkin@gmail.com

--- pages/fitness.js
+++ pages/fitness.js
@@ -1,50 +1,60 @@
-import NavBar from '../components/NavBar/NavBar.js';
-import ActivityButton from '../components/ActivityButton/ActivityButton.js';
-import TimeButton from '../components/TimeButton/TimeButton.js';
-import { useState } from 'react';
-import Link from 'next/link';
-import { Box, Typography, Grid, Button } from '@mui/material';
-import { dummyFitness, times } from '../DummyData/DummyFitnessData.js';
-import Calendar from '../components/Calendar/Calendar.js';
+import NavBar from "../components/NavBar/NavBar.js";
+import ActivityButton from "../components/ActivityButton/ActivityButton.js";
+import TimeButton from "../components/TimeButton/TimeButton.js";
+import { useState } from "react";
+import Link from "next/link";
+import { Box, Typography, Grid, Button } from "@mui/material";
+import { dummyFitness, times } from "../DummyData/DummyFitnessData.js";
+import Calendar from "../components/Calendar/Calendar.js";
 
 const Fitness = () => {
-	const [ fitnessInfo, setFitnessInfo ] = useState();
+  const [fitnessInfo, setFitnessInfo] = useState({
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+  });
 
-	console.log(fitnessInfo);
+  console.log(fitnessInfo);
 
-	return (
-		<div>
-			<NavBar />
+  return (
+    <div>
+      <NavBar />
 
-			<h1 className=" fitnessbg ">Fitness</h1>
-			
-			<Calendar setFitnessInfo={setFitnessInfo}/>
-			<Grid container>
-				{fitnessInfo === undefined ? (
-					dummyFitness.map((exercise) => (
-						<ActivityButton
-							title={exercise.title}
-							category={exercise.category}
-							description={exercise.description}
-							key={exercise.title}
-							setFitnessInfo={setFitnessInfo}
-							image={exercise.image}
-						/>
-					))
-				) : (
-					times.map((time, index) => (
-						<TimeButton time={time} key={index} setFitnessInfo={setFitnessInfo} fitnessInfo={fitnessInfo} />
-					))
-				)}
-			</Grid>
+      <h1 className=" fitnessbg ">Fitness</h1>
 
-			<Link href="/">
-				<div className="backBtn">
-					<Typography> Back </Typography>
-				</div>
-			</Link>
-		</div>
-	);
+      <Calendar setFitnessInfo={setFitnessInfo} fitnessInfo={fitnessInfo} />
+      <Grid container>
+        {fitnessInfo.title === ""
+          ? dummyFitness.map((exercise) => (
+              <ActivityButton
+                title={exercise.title}
+                category={exercise.category}
+                description={exercise.description}
+                key={exercise.title}
+                setFitnessInfo={setFitnessInfo}
+                image={exercise.image}
+                fitnessInfo={fitnessInfo}
+                // date={date}
+              />
+            ))
+          : times.map((time, index) => (
+              <TimeButton
+                time={time}
+                key={index}
+                setFitnessInfo={setFitnessInfo}
+                fitnessInfo={fitnessInfo}
+              />
+            ))}
+      </Grid>
+
+      <Link href="/">
+        <div className="backBtn">
+          <Typography> Back </Typography>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Fitness;
