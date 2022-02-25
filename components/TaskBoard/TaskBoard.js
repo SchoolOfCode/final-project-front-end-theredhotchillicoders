06170daf50a0f7037352c693ca11919c97d285b8
emilysarina <emy.sarina@gmail.com>
2022-02-25 16:25:50 +0000

    started task calendar

--- components/TaskBoard/TaskBoard.js
+++ components/TaskBoard/TaskBoard.js
@@ -1,18 +1,33 @@
-import React from "react";
+import {React , useState} from "react";
 import css from "./Taskboard.module.css";
 import AddTask from "../addTaskButton/addTask.js";
 import Todo from "../Task/Task";
+import TaskCalendar from "../TaskCalendar/TaskCalendar";
+import moment from "moment";
+
+/*
+task date in usestate on the index, thats what needs to change
+pass down set date into calender 
+use date to filter tasks
+const [value, setValue] = useState(new Date());
+*/
 
 
-
-export default function TaskBoard({ todos , deleteItem, deleteRequest}) {
-  console.log(todos);
-
+export default function TaskBoard({ todos , deleteItem, deleteRequest }) {
+  const [taskDate, setTaskDate] = useState(new Date());
+  console.log("moment date" , moment(taskDate).format('YYYY-MM-DD'))
+  ;
+ 
+ console.log(taskDate)
   if (todos.length > 0) {
+    console.log(todos[8].date)
+    console.log("data date" , String(todos[8].date).substring(0,10))
+    //console.log(taskDate)
     return (
       <div className={css.taskboard}>
+          <TaskCalendar taskDate={taskDate} setTaskDate={setTaskDate}/>
         <div className={css.todoList}>
-          {todos.map((todo, index ) => {
+          {todos.filter(todos => {String((todos.date)).substring(0,10) === String(moment(taskDate).format('YYYY-MM-DD'))}).map((todo, index ) => {
             function deleteTaskOnClick(){
               deleteItem(index)
               deleteRequest(todo.id)}
@@ -34,6 +49,7 @@ export default function TaskBoard({ todos , deleteItem, deleteRequest}) {
 }
 
 /*
+filter(todos => {String(todos.date).substring(0,10) == String(taskDate)})
  function handleDelete(i) {
     console.log("%chandle delete", "color:lightblue");
     setToDos([...toDos.slice(0, i), ...toDos.slice(i + 1)]);
---
+++ components/TaskCalendar/TaskCalendar.js
@@ -0,0 +1,24 @@
+import {React , useState } from 'react'
+import TextField from "@mui/material/TextField";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+
+export default function TaskCalendar({taskDate, setTaskDate}) {
+    
+    function handleChange(newValue) {
+        setTaskDate(newValue);
+      }
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DatePicker
+            label="What day are you planning?"
+            value={taskDate}
+            onChange={(newValue) => {
+              handleChange(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      );
+}
