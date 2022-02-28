d5d36e6992b308aa616598c8ff8a05a4301f2d75
Leanne251 <leannesmith251@gmail.com>
2022-02-28 15:00:40 +0000

    start of progress bar

--- components/Task/Task.js
+++ components/Task/Task.js
@@ -1,26 +1,50 @@
-
-import React, { useState } from 'react';
+import { React, useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
-import css from "./task.module.css";
+import css from './task.module.css';
 
-export default function Todo({todo , id , deleteTaskOnClick}) {
-   let category = todo.category
-    
-    return (
-      <div className={css.todo} id={category}>
-        {todo.title}
-        <div>
-          <input className={css.checkbox} type="checkbox" />
-          <IconButton aria-label="delete" size="small" onClick={deleteTaskOnClick}>
-          <DeleteIcon fontSize="small"  />
-          </IconButton>
-        </div>
+export default function Todo({ todo, id, deleteTaskOnClick }) {
+	let category = todo.category;
 
-      </div>
-  );
+	async function sendPatchRequest(id) {
+		const newBoolean = !todo.isComplete;
+
+		const response = await fetch(`https://socfinalproject.herokuapp.com/activities/${id}`, {
+			method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+			mode: 'cors', // no-cors, *cors, same-origin
+			// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			// credentials: 'same-origin', // include, *same-origin, omit
+			headers: {
+				'Content-Type': 'application/json'
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+
+			//redirect: 'follow', // manual, *follow, error
+			//referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+			body: JSON.stringify({ isComplete: newBoolean }) // body data type must match "Content-Type" header
+		});
+		let data = await response.json();
+		console.log(response, data);
+		// parses JSON response into native JavaScript objects
+	}
+
+	return (
+		<div className={css.todo} id={category}>
+			{todo.title}
+			<div>
+				<input
+					className={css.checkbox}
+					type="checkbox"
+					onClick={() => {
+						sendPatchRequest(id);
+					}}
+				/>
+				<IconButton aria-label="delete" size="small" onClick={deleteTaskOnClick}>
+					<DeleteIcon fontSize="small" />
+				</IconButton>
+			</div>
+		</div>
+	);
 }
 
-
-
- 
+// onClick send the patch request to update the database
