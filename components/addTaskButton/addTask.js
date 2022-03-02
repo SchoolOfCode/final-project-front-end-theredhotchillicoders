82d8d14b0313dbf48fdf590f3aae17d5f6c97c90
Leanne251 <leannesmith251@gmail.com>
2022-03-02 14:53:49 +0000

    task board buttons Co-authored-by: Leanne leannesmith251@gmail.com Co-authored-by: Tan tanmatthews1@gmail.com Co-authored-by: Emily emy.sarina@gmail.com Co-authored-by: Mohammed ali.m2804@outlook.com Co-authored-by: troy troywebb95@gmail.com Co-authored-by: jack jackfirkin@gmail.com

--- components/addTaskButton/addTask.js
+++ components/addTaskButton/addTask.js
@@ -1,18 +1,62 @@
-import { useState } from "react";
-import css from "./addTaskButton.module.css";
+import { useState } from 'react';
+import css from './addTaskButton.module.css';
+import { Button, Modal, Typography, Box } from '@mui/material/';
+import Link from 'next/link';
+// const style = {
+// 	position: 'absolute',
+// 	top: '50%',
+// 	left: '50%',
+// 	transform: 'translate(-50%, -50%)',
+// 	width: 400,
+// 	bgcolor: 'background.paper',
+// 	border: '2px solid #000',
+// 	boxShadow: 24,
+// 	p: 4
+// };
 
 export default function AddTask() {
-  const [text, setText] = useState("");
+	const [ open, setOpen ] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-      <button className={css.taskButton}>+ Add Task</button>
-      {/* <input
-                type='text'
-                placeholder="Add Task" 
-                value = {text}
-                onChange={(e) => setText(e.target.value)}
-                /> */}
-    </div>
-  );
+	return (
+		<div>
+			<button className={css.taskButton}>+ Add Task</button>
+
+			<Button onClick={handleOpen}>+ Add Task</Button>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-modal-title"
+				aria-describedby="modal-modal-description"
+			>
+				<div className={css.style}>
+					<h3>What do you want to do today?</h3>
+					<Link href="/fitness">
+						<a className={css.activityButton} id="fitness">
+							Fitness
+						</a>
+					</Link>
+					<Link href="/recipes">
+						<a className={css.activityButton} id="recipes">
+							Recipes
+						</a>
+					</Link>
+					<Link href="/goals">
+						<a className={css.activityButton} id="myGoals">
+							Goals
+						</a>
+					</Link>
+					<Link href="/wellbeing">
+						<a className={css.activityButton} id="wellness">
+							Wellbeing
+						</a>
+					</Link>
+					<button onClick={handleClose} className={css.backButton}>
+						Back
+					</button>
+				</div>
+			</Modal>
+		</div>
+	);
 }
---
+++ pages/wellbeing.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function wellbeing() {
+	return <div>wellbeing</div>;
+}
+
+export default wellbeing;
