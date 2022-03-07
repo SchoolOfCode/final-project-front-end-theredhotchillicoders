bb5ad86a2bad5dcaa05d604d2f399b1a41f3420f
Pokemom3 <tanmatthews1@gmail.com>
2022-03-07 10:58:00 +0000

    Added dummy goal data

--- DummyData/DummyGoalsData.js
+++ DummyData/DummyGoalsData.js
@@ -1,41 +1,24 @@
 export const dummyGoals = [
-	{
-		title: 'Yoga',
-		category: 'myGoals',
-		description: 'For more steps go here...',
-		duration: '',
-		image: '/wellbeing/yoga.svg'
-	},
-	{
-		title: 'Meditation',
-		category: 'myGoals',
-		description: 'Clear your mind...',
-		image: '/wellbeing/meditation.svg'
-	},
-	{
-		title: 'Call a friend',
-		category: 'myGoals',
-		description: 'Good for you and the friend!',
-		image: '/wellbeing/callAFriend.svg'
-	},
-	{
-		title: 'Gardening',
-		category: 'myGoals',
-		description: 'Bonus points for growing vegetables',
-		image: '/wellbeing/gardening.svg'
-	},
-	{
-		title: 'Go outside',
-		category: 'myGoals',
-		description: 'Get some fresh air! Bonus points if you take a dog with you',
-		image: '/wellbeing/goOutside.svg'
-	},
-	{
-		title: 'Read a book',
-		category: 'myGoals',
-		description: 'Stop looking at a screen!',
-		image: '/wellbeing/reading.svg'
-	}
-];
+	
+		{
+		title: 'Drink water',
+				category: 'myGoals',
+				description: '1 cup 5 times',
+				image: '',
+		},
+		{
+		title: 'Walk 500 steps',
+				category: 'myGoals',
+				description: 'Take a walk outside',
+				image: '',
+		},
+		{
+		title: 'Ten breaths',
+				category: 'myGoals',
+				description: 'Fill your stomach with air like a balloon',
+				image: '',
+		},
+	]
+		
 
-export const times = [ '15 mins', '30mins', '45mins', '1 hours' ];
+export const times = [ '3 x daily', '4 x daily', '5 x daily', '6 x daily' ];
