238ed2923a7937e4263a05ae284b52dc666334f6
Pokemom3 <tanmatthews1@gmail.com>
2022-03-07 11:44:40 +0000

    Added images for goals page Co-authored-by: Leanne leannesmith251@gmail.com

--- DummyData/DummyGoalsData.js
+++ DummyData/DummyGoalsData.js
@@ -1,24 +1,34 @@
 export const dummyGoals = [
-	
-		{
-		title: 'Drink water',
-				category: 'myGoals',
-				description: '1 cup 5 times',
-				image: '',
-		},
-		{
-		title: 'Walk 500 steps',
-				category: 'myGoals',
-				description: 'Take a walk outside',
-				image: '',
-		},
-		{
-		title: 'Ten breaths',
-				category: 'myGoals',
-				description: 'Fill your stomach with air like a balloon',
-				image: '',
-		},
-	]
-		
+    {
+        title: 'Drink water',
+        category: 'myGoals',
+        description: '1 cup 5 times',
+        image: '/goals/drinkWater.svg',
+    },
+    {
+        title: '500 steps',
+        category: 'myGoals',
+        description: 'Take a walk outside',
+        image: '/goals/walking.svg',
+    },
+    {
+        title: '10 breaths',
+        category: 'myGoals',
+        description: 'Fill your stomach with air like a balloon',
+        image: '/goals/breathing.svg',
+    },
+    {
+        title: 'Eat leafy veg',
+        category: 'myGoals',
+        description: 'Eat a fist full of leafy green vegetables',
+        image: '/goals/leafyGreenVeg.svg',
+    },
+    {
+        title: 'Think positive',
+        category: 'myGoals',
+        description: 'Say one positive thing about yourself',
+        image: '/goals/happy.svg',
+    },
+]
 
-export const times = [ '3 x daily', '4 x daily', '5 x daily', '6 x daily' ];
+export const times = ['3 x daily', '4 x daily', '5 x daily', '6 x daily']
