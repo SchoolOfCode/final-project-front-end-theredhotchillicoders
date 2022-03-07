24fcacd391f30f8f53d531bfbba9aec3f213efaf
Leanne251 <leannesmith251@gmail.com>
2022-03-07 15:45:31 +0000

    working post for own event

--- pages/goals.js
+++ pages/goals.js
@@ -1,80 +1,72 @@
-import ActivityButton from '../components/ActivityButton/ActivityButton.js'
-import TimeButton from '../components/TimeButton/TimeButton.js'
-import { useState } from 'react'
-import Link from 'next/link'
+import ActivityButton from '../components/ActivityButton/ActivityButton.js';
+import TimeButton from '../components/TimeButton/TimeButton.js';
+import { useState } from 'react';
+import Link from 'next/link';
 
-import { Typography, Grid } from '@mui/material'
-import { dummyGoals, times } from '../DummyData/DummyGoalsData'
-import Calendar from '../components/Calendar/Calendar.js'
-import AddYourOwn from '../components/AddYourOwn/AddYourOwn'
+import { Typography, Grid } from '@mui/material';
+import { dummyGoals, times } from '../DummyData/DummyGoalsData';
+import Calendar from '../components/Calendar/Calendar.js';
+import AddYourOwn from '../components/AddYourOwn/AddYourOwn';
 
-
-
-const date = new Date()
+const date = new Date();
 const Goals = ({ user }) => {
-    const [GoalsInfo, setGoalsInfo] = useState({
-        date: date,
-        title: '',
+	const [ GoalsInfo, setGoalsInfo ] = useState({
+		date: date,
+		title: '',
+		category: 'myGoals',
+		description: '',
+		userid: user.uid
+	});
 
-        category: 'myGoals',
+	// function getTime(e) {
+	//   setFitnessInfo({ ...fitnessInfo, duration: e.target.innerHTML });
+	//   sendPostRequest(fitnessInfo);
+	// }
 
+	return (
+		<div>
+			<h1 className=" goalsbg ">Personal Goals</h1>
+			<div className="activity-calander">
+				<Calendar setInfo={setGoalsInfo} Info={GoalsInfo} />
+			</div>
+			<Grid container>
+				{GoalsInfo.title === '' ? <AddYourOwn info={GoalsInfo} setInfo={setGoalsInfo} /> : null}
 
-        description: '',
-        userid: user.uid,
-    })
+				{GoalsInfo.title === '' ? (
+					dummyGoals.map((goals) => (
+						<ActivityButton
+							title={goals.title}
+							category={goals.category}
+							description={goals.description}
+							key={goals.title}
+							setInfo={setGoalsInfo}
+							image={goals.image}
+							Info={GoalsInfo}
+							// date={date}
+						/>
+					))
+				) : (
+					times.map((time, index) => (
+						<TimeButton
+							time={time}
+							key={index}
+							setInfo={setGoalsInfo}
+							Info={GoalsInfo}
+							category="myGoals"
+						/>
+					))
+				)}
+			</Grid>
 
-    // function getTime(e) {
-    //   setFitnessInfo({ ...fitnessInfo, duration: e.target.innerHTML });
-    //   sendPostRequest(fitnessInfo);
-    // }
+			<Link href="/">
+				<a>
+					<div className="backBtn">
+						<Typography> Back </Typography>
+					</div>
+				</a>
+			</Link>
+		</div>
+	);
+};
 
-    return (
-        <div>
-            <h1 className=" goalsbg ">Personal Goals</h1>
-            <div className="activity-calander">
-                <Calendar setInfo={setGoalsInfo} Info={GoalsInfo} />
-            </div>
-            <Grid container>
-
-			{GoalsInfo.title === '' ?
-                <AddYourOwn info={GoalsInfo} setInfo={setGoalsInfo} />: null}
-				
-
-                {GoalsInfo.title === ''
-                    ? dummyGoals.map((goals) => (
-                          <ActivityButton
-                              title={goals.title}
-                              category={goals.category}
-                              description={goals.description}
-                              key={goals.title}
-                              setInfo={setGoalsInfo}
-                              image={goals.image}
-                              Info={GoalsInfo}
-                              // date={date}
-                          />
-                      ))
-                    : times.map((time, index) => (
-                          <TimeButton
-                              time={time}
-                              key={index}
-                              setInfo={setGoalsInfo}
-                              Info={GoalsInfo}
-
-                              category="myGoals"
-
-                          />
-                      ))}
-            </Grid>
-
-            <Link href="/">
-                <a>
-                    <div className="backBtn">
-                        <Typography> Back </Typography>
-                    </div>
-                </a>
-            </Link>
-        </div>
-    )
-}
-
-export default Goals
+export default Goals;
